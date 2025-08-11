38664b750af5d0465eab8ec80f9747a2f18ea70e
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2025-08-11 15:06:39 -0300

    feat: se agrago funcion para agregar nombres

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let amigos = [];
+
+const limpiar = id => document.getElementById(id).value = "";
+
+function agregarAmigo() {
+  const amigo = document.getElementById("amigo").value;
+  
+  amigo 
+  ? (amigos.push(amigo), limpiar("amigo")) 
+  : alert("Por favor debe ingresar un nombre valido");
+
+  console.log(amigos);
+}
