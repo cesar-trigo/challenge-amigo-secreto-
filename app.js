d8f44bdb00b18b25a3c6bb101817a66e741bb141
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2025-08-11 16:46:33 -0300

    feat: implemantacion para actualizar y mostrar lista de amigos

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 let amigos = [];
+let $listaAmigos = document.getElementById("listaAmigos");
 
 const limpiar = id => document.getElementById(id).value = "";
 
 function agregarAmigo() {
   const amigo = document.getElementById("amigo").value;
-  
-  amigo 
-  ? (amigos.push(amigo), limpiar("amigo")) 
-  : alert("Por favor, inserte un nombre.");
+
+  amigo
+    ? (amigos.push(amigo), limpiar("amigo"), imprimirNombre(amigos, $listaAmigos))
+    : alert("Por favor, inserte un nombre.");
 
   console.log(amigos);
+}
+
+function crearNombre(e) {
+  let li = document.createElement("li");
+  li.textContent = e;
+  return li;
+}
+
+function imprimirNombre(e, contenedor) {
+  contenedor.innerHTML = "";
+
+  for (i = 0; i < e.length; i++) {
+    const text = crearNombre(e[i]);
+    contenedor.appendChild(text);
+  }
+
 }
